bbb769a26ac9668c9600b6124abcf54073570a38
joachimprinzbach <joachim.prinzbach@gmail.com>
2016-12-26 22:18:48 +0100

    add zip build

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 var path = require('path');
+var ZipPlugin = require('zip-webpack-plugin');
 
 module.exports = {
 
-    entry: './index.js',
+    entry: {
+        'app': './index.js'
+    },
 
     output: {
         filename: '[name].bundle.js',
         path: path.join(__dirname, 'dist')
-    }
+    },
+
+    plugins: [
+        new ZipPlugin({
+            filename: 'app.bundle.zip'
+        })
+    ]
 }
